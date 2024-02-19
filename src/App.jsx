8e1f573ce17d2c8a33079b7b9dfac16ded2d53f8
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-02-19 11:13:44 +0100

    created routes and main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
-import { useState } from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './components/Home'
+import AboutMe from './components/AboutMe'
+import Projects from './components/Projects'
+import Resume from './components/Resume'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+
 
 import './App.css'
 
@@ -7,7 +14,15 @@ function App() {
 
   return (
     <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<AboutMe />} />
+      <Route path="projects" element={<Projects />} />
+      <Route path="resume" element={<Resume />} />
+    </Routes>
+    <Nav />
       <h1>Hello Portfolio</h1>
+    <Footer />
     </>
   )
 }

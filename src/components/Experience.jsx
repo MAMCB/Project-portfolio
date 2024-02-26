ecb6782b978b00081b1ba07002303db82b22e3c4
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-02-26 16:18:46 +0100

    typo fix

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,7 +8,7 @@ import LogCameraPosition from "./LogCameraPosition";
 
 const Experience = () => {
   return (
-    <>_
+    <>
       <PerspectiveCamera
         position={[1.4233913844722013, -5.7173277105425128, -6.288373349251435]} // Set the initial position of the camera
         rotation={[3.0894158228185757,-0.8048316906356017,3.1039716789266847]} // Set the initial rotation of the camera

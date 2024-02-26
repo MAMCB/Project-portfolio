117a0296549d3f140a66d327dd3d3282a367b1de
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-02-26 13:02:53 +0100

    basic three.js setup with cube

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,47 @@
-import {BrowserRouter,Routes, Route} from 'react-router-dom'
-import Home from './components/Home'
-import AboutMe from './components/AboutMe'
-import Projects from './components/Projects'
-import Resume from './components/Resume'
-import NavBar from './components/NavBar'
-import MyFooter from './components/MyFooter'
-import ProjectDetail from './components/ProjectDetail'
-import Contacts from './components/Contacts'
-import AboutPortfolio from './components/AboutPortfolio'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import Resume from "./components/Resume";
+import NavBar from "./components/NavBar";
+import MyFooter from "./components/MyFooter";
+import ProjectDetail from "./components/ProjectDetail";
+import Contacts from "./components/Contacts";
+import AboutPortfolio from "./components/AboutPortfolio";
 import { DarkThemeToggle, Flowbite } from "flowbite-react";
+import { Canvas } from "@react-three/fiber";
+import Experience from "./components/Experience";
 
+import "./App.css";
 
-import './App.css'
 
 function App() {
-
-
   return (
     <>
-    <Flowbite>
-      <NavBar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutMe />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path='/projects/:id' element={<ProjectDetail />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/about-portfolio" element={<AboutPortfolio />} />
-        </Routes>
-      </BrowserRouter>
-      
-      
-      <MyFooter />
-    </Flowbite>
+      <Flowbite>
+        <NavBar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutMe />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/projects/:id" element={<ProjectDetail />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/about-portfolio" element={<AboutPortfolio />} />
+          </Routes>
+        </BrowserRouter>
+        <div className="App">
+          <Canvas>
+            <Experience />
+           
+          </Canvas>
+        </div>
 
+        <MyFooter />
+      </Flowbite>
     </>
   );
 }
 
-export default App
+export default App;
